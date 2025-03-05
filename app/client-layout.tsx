900f370959a99e2518287fd66061e0d49c720e5c
kristenhill12 <kristenhillwong@gmail.com>
2025-03-04 16:12:47 -0800

    Fixed navigation bar links and styling

--- app/client-layout.tsx
+++ app/client-layout.tsx
@@ -15,24 +15,36 @@ export default function ClientLayout({
 }) {
   const pathname = usePathname()
   const [isFirstLoad, setIsFirstLoad] = useState(true)
+  const [hasVisitedBefore, setHasVisitedBefore] = useState(false)
   
   useSmoothScroll()
   
   useEffect(() => {
+    // Check if this is the very first load of the site
     if (isFirstLoad) {
       const timer = setTimeout(() => {
         setIsFirstLoad(false)
+        // Set a flag that the user has visited before
+        setHasVisitedBefore(true)
+        // Optionally, also store in sessionStorage for persistence across refreshes
+        sessionStorage.setItem('hasVisitedBefore', 'true')
       }, 2000)
       
       return () => clearTimeout(timer)
     }
   }, [isFirstLoad])
   
+  // This effect runs when pathname changes
+  useEffect(() => {
+    // We don't need to do anything special here - by NOT setting isFirstLoad
+    // to true when returning to homepage, we prevent the preloader from showing again
+  }, [pathname])
+  
   return (
     <>
       <NavBar />
       <AnimatePresence mode="wait">
-        {isFirstLoad && pathname === "/" ? (
+        {isFirstLoad && pathname === "/" && !hasVisitedBefore ? (
           <Preloader key="preloader" />
         ) : (
           <PageTransition key={pathname}>{children}</PageTransition>
