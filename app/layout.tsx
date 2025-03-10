8311d5a52992b0b20e095ae744f850d6abc73595
kristenhill12 <kristenhillwong@gmail.com>
2025-03-10 11:57:11 -0700

    revet layout and client layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
   description: "UX/UI Designer & Developer Portfolio",
 };
 
-export default function RootLayout({
+export default function Layout({
   children,
 }: {
   children: React.ReactNode;
